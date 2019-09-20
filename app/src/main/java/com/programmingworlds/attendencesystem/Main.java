c2b1404873404c4d3178a793647530cc40cea040
MOHIT AGRAWAL <nicemohit260@gmail.com>
2019-09-20 22:55:51 +0530

    CREATED ATTENDENCE REPORT LAYOUT
    Signed-off-by: MOHIT AGRAWAL <nicemohit260@gmail.com>

--- app/src/main/java/com/programmingworlds/attendencesystem/Main.java
+++ app/src/main/java/com/programmingworlds/attendencesystem/Main.java
@@ -1,5 +1,5 @@
 package com.programmingworlds.attendencesystem;
-/*
+
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -21,7 +21,7 @@ public class Main extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
 
-   /* @Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -36,7 +36,7 @@ public class Main extends AppCompatActivity {
             }
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
-      /*  NavigationView navigationView = findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
@@ -44,12 +44,12 @@ public class Main extends AppCompatActivity {
                 R.id.nav_tools, R.id.nav_share, R.id.nav_send)
                 .setDrawerLayout(drawer)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+      //  NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+      //  NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+      //  NavigationUI.setupWithNavController(navigationView, navController);
     }
-*/
-      /*
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -57,12 +57,11 @@ public class Main extends AppCompatActivity {
         return true;
     }
 
-    @Override
+  /*  @Override
     public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
+       // NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+       // return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+        //        || super.onSupportNavigateUp();
     }
-
-}
 */
+}
